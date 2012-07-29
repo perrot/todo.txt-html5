7c4789b8a8d11251b547b0c5426b09b37dc47985
Andrew Oberstar <ajoberstar@gmail.com>
2012-07-29 18:35:01 -0500

    Added initial parser for todo.txt.

---
+++ src/main/web/todo-parser.js
@@ -0,0 +1,39 @@
+function parseTodo(contents) {
+	var lines = contents.split('\n');
+	var tasks = Array();
+	for (i = 0; i < lines.length; i++) {
+		var pattern = /^\s*(?:(x)\s+)?(?:(\d{4}-\d{2}-\d{2})\s+)?(?:\(([A-Za-z])\)\s+)?(?:(\d{4}-\d{2}-\d{2})\s+)?(.*)/;
+		var matches = pattern.exec(lines[i]);
+		
+		var task = { }
+		task.complete = matches[1] == 'x';
+		task.priority = matches[3];
+		task.description = matches[5];
+		
+		if (matches[3] || matches[4]) {
+			task.created = matches[4];
+			task.completed = matches[2];
+		} else {
+			task.created = matches[2];
+			task.completed = undefined;
+		}
+		
+		task.contexts = parseContexts(matches[5]);
+		task.projects = parseProjects(matches[5]);
+		tasks[i] = task;
+		//alert(task);
+		console.debug("%o", task);
+	}
+	return tasks;
+}
+
+function parseContexts(description) {
+	var pattern = /@\w+/;
+	return pattern.exec(description);
+}
+
+function parseProjects(description) {
+	var pattern = /\+\w+/;
+	return pattern.exec(description);
+}
+
