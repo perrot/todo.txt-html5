df70acdb95986abad8c0e3ae10c0789a399f1d5c
Andrew Oberstar <ajoberstar@gmail.com>
2012-08-15 22:05:47 -0500

    Moved drag and drop JS and CSS to separate files.

---
+++ src/main/web/drag-n-drop.js
@@ -0,0 +1,35 @@
+function handleDnD(event) {
+	event.stopPropagation();
+	event.preventDefault();
+}
+
+$(document).ready(function() {
+	var dropbox = $('#dropbox')
+	dropbox.bind('dragover', function(event) {
+		handleDnD(event);
+		$('#dropbox').addClass('over');
+	});
+	dropbox.bind('dragleave', function(event) {
+		handleDnD(event);
+		$('#dropbox').removeClass('over');
+	});
+	dropbox.bind('drop', function(event) {
+		handleDnD(event);
+		var reader = new FileReader();
+		reader.onload = function(event) {
+			var contents = $('#contents');
+			contents.text('');
+			contents.append('<pre>' + event.target.result + '</pre>');
+		}
+		reader.onerror = function(event) {
+			console.log('file read error', e);
+		}
+		
+		var files = event.originalEvent.dataTransfer.files;
+		for (var i = 0; i < files.length; i++) {
+			$('#name').text(files[i].name);
+			reader.readAsText(files[i]);
+		}
+		$('#dropbox').removeClass('over');
+	});
+});
